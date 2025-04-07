f59186e30df3a4b6c965cb7a47fb7450ff8a6a4a
greyw01f <83853649+greyw01f@users.noreply.github.com>
2025-04-07 00:05:28 -0400

    Add files via upload

---
+++ js/stipt.js
@@ -0,0 +1,18 @@
+const sticker01 = document.getElementById('dato01')
+const sticker02 = document.getElementById('dato02')
+const sticker03 = document.getElementById('dato03')
+const botonSticker = document.getElementById('mostrarDato')
+const muestraValor = document.getElementById('muestra-stickers')
+
+botonSticker.addEventListener('click',function() {
+    let valor01 = parseInt(sticker01.value) 
+    let valor02 = parseInt(sticker02.value)
+    let valor03 = parseInt(sticker03.value)
+    let valorTotal = Number(valor01 + valor02 + valor03)
+    
+    if(valorTotal<10){
+        muestraValor.innerHTML = "LLevas :" + "" + valorTotal + "" +"Stickers"
+    }else{
+        muestraValor.innerHTML = "LLevas demasiados Stickers"
+    }
+})
